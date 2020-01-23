50434b6f1792bad3cddbf3a195695392b3a6de76
hemant <nhemnt@gmail.com>
2020-01-24 03:15:06 +0530

    add first useStaticQuery

--- src/examples/Header.js
+++ src/examples/Header.js
@@ -1,10 +1,23 @@
 import React from "react"
+import { useStaticQuery, graphql } from 'gatsby';
 
 export const Header = () => {
+  const { site } = useStaticQuery(graphql`{
+    site {
+      siteMetadata {
+        title
+        author
+        data {
+          age
+        }
+      }
+    }
+  }`)
+  console.log(site);
   return (
     <div>
       <h1>Header</h1>
-      <h1>Title</h1>
+      <h1>Title: {site.siteMetadata.title}</h1>
     </div>
   )
 }
